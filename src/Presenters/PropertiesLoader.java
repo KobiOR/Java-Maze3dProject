8617bae6e8cf75c0ecb660e2b41ffc1fe95a1737
KobiOR <orr.kobi@gmail.com>
2016-09-23 14:23:30 +0300

    update

--- src/Presenters/PropertiesLoader.java
+++ src/Presenters/PropertiesLoader.java
@@ -8,26 +8,18 @@ import static mazeGenerators.Maze3dGenerator.ANSI_BOLD;
 import static mazeGenerators.Maze3dGenerator.ANSI_RED;
 import static mazeGenerators.Maze3dGenerator.ANSI_RESET;
 
-/**
- * Load the properties from the xml file "properties" at the start of the program
- * @author Tomer, Gilad
- *
- */
+
+
 public class PropertiesLoader {
     private static PropertiesLoader instance;
     private Properties properties;
 
-    /**
-     * Get the properties list
-     * @return
-     */
+
     public Properties getProperties() {
         return properties;
     }
 
-    /**
-     * CTOR
-     */
+
     private PropertiesLoader()
     {
         try {
@@ -39,10 +31,7 @@ public class PropertiesLoader {
         }
     }
 
-    /**
-     * Get the instance of the properties loader
-     * @return
-     */
+
     public static PropertiesLoader getInstance() {
         if (instance == null)
             instance = new PropertiesLoader();
